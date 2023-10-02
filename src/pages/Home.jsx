1717983bfc26077c750cb6cdd05e8e3ceef806eb
Daiki4529 <jonasbarth.lyceealain@gmail.com>
2023-10-02 09:35:19 +0200

    Root Formatting for a common size ofr all pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,20 +2,13 @@ import React from "react";
 
 function Home() {
   return (
-    <div id="log_in">
-      <form>
-        <p className="form_title">Connexion</p>
-        <input
-          type="text"
-          name="firstname"
-          id="firstname"
-          placeholder="Prénom"
-        />
-        <input type="text" name="lastname" id="lastname" placeholder="Nom" />
-        <input type="submit" value="Se Connecter" />
-        <div className="bg best"></div>
-      </form>
-    </div>
+    <form>
+      <p className="form_title">Connexion</p>
+      <input type="text" name="firstname" id="firstname" placeholder="Prénom" />
+      <input type="text" name="lastname" id="lastname" placeholder="Nom" />
+      <input type="submit" value="Se Connecter" />
+      <div className="bg best"></div>
+    </form>
   );
 }
 export default Home;

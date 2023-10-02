75381c312b68bfb1fcefdd54ca005c53d75664a4
Daiki4529 <jonasbarth.lyceealain@gmail.com>
2023-10-02 13:50:33 +0200

    Form Submission first commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Home() {
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+
+  const handleLogin = () => {
+    if (firstname && lastname) {
+    }
+  };
+
   return (
-    <form>
+    <form onSubmit={handleLogin}>
       <p className="form_title">Connexion</p>
-      <input type="text" name="firstname" id="firstname" placeholder="Prénom" />
-      <input type="text" name="lastname" id="lastname" placeholder="Nom" />
+      <input
+        type="text"
+        name="firstname"
+        id="firstname"
+        placeholder="Prénom"
+        value={firstname}
+        onChange={(e) => setFirstname(e.target.value)}
+        required
+      />
+      <input
+        type="text"
+        name="lastname"
+        id="lastname"
+        placeholder="Nom"
+        value={lastname}
+        onChange={(e) => setLastname(e.target.value)}
+        required
+      />
       <input type="submit" value="Se Connecter" />
       <div className="bg best"></div>
     </form>

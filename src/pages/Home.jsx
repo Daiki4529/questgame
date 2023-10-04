e68cf087296cf3a0a142c381d3c37a957b50558c
Daiki4529 <jonasbarth.lyceealain@gmail.com>
2023-10-04 12:25:23 +0200

    Re-Organized divs, Updated the render to react 1.8, removed analytics, added a redirection on login

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,43 +1,68 @@
 import React, { useState } from "react";
+import Swal from "sweetalert2";
 import { useNavigate } from "react-router-dom";
 import { collection, getDocs } from "firebase/firestore";
+import { db } from "../config/firestore";
 
 function Home() {
   const [firstname, setFirstname] = useState("");
   const [lastname, setLastname] = useState("");
-
   const navigate = useNavigate();
-  const handleLogin = () => {
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
     if (firstname && lastname) {
-      navigate("/profile");
-      alert(`${firstname} ${lastname} a tenté de se connecter en vain.`);
+      const querySnapshot = await getDocs(collection(db, "users"));
+      const users = querySnapshot.docs.map((doc) => doc.data());
+
+      let userExists = false;
+
+      for (var i = 0; i < users.length; i++) {
+        const user = users[i];
+        if (user.firstname === firstname && user.lastname === lastname) {
+          userExists = true;
+          break;
+        }
+      }
+
+      if (userExists) {
+        navigate(`/profile?firstname=${firstname}&lastname=${lastname}`);
+      } else {
+        Swal.fire({
+          icon: "error",
+          title: "Erreur",
+          text: `${firstname} ${lastname} n'existe pas.`,
+        });
+      }
     }
   };
-
   return (
-    <form onSubmit={handleLogin}>
-      <p className="form_title">Connexion</p>
-      <input
-        type="text"
-        name="firstname"
-        id="firstname"
-        placeholder="Prénom"
-        value={firstname}
-        onChange={(e) => setFirstname(e.target.value)}
-        required
-      />
-      <input
-        type="text"
-        name="lastname"
-        id="lastname"
-        placeholder="Nom"
-        value={lastname}
-        onChange={(e) => setLastname(e.target.value)}
-        required
-      />
-      <input type="submit" value="Se Connecter" />
-      <div className="bg best"></div>
-    </form>
+    <div id="homeContainer">
+      <form onSubmit={handleLogin}>
+        <p className="form_title">Connexion</p>
+        <input
+          type="text"
+          name="firstname"
+          id="firstname"
+          placeholder="Prénom"
+          value={firstname}
+          onChange={(e) => setFirstname(e.target.value)}
+          required
+        />
+        <input
+          type="text"
+          name="lastname"
+          id="lastname"
+          placeholder="Nom"
+          value={lastname}
+          onChange={(e) => setLastname(e.target.value)}
+          required
+        />
+        <input type="submit" value="Se Connecter" />
+        <div className="bg best"></div>
+      </form>
+    </div>
   );
 }
 export default Home;
